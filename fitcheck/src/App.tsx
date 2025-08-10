e1c52f9d0a9bae0b7f2795a02bdc6d697ae50db3
himavanth_karpurapu <charipikaindigo@gmail.com>
2025-08-10 07:40:04 -0700

    Add preferences page with Shopify Badge components - single selection for all categories

--- fitcheck/src/App.tsx
+++ fitcheck/src/App.tsx
@@ -8,6 +8,7 @@ import {
 import { Welcome } from "./pages/welcome";
 import Saved from "./pages/saved";
 import Home from "./pages/home";
+import Preferences from "./pages/preferences";
 import NavBar from "./components/Navbar";
 import Shop from "./pages/tryon/shop";
 import YourFit from "./pages/tryon/yourfit";
@@ -24,14 +25,11 @@ function AppRoutes() {
         <Link to="/" className="mr-4">
           Welcome
         </Link>
-        <Link to="/home" className="mr-4">
-          Home
-        </Link>
-        <Link to="/tryon" className="mr-4">
-          Try On
-        </Link>
-        <Link to="/saved">Saved</Link>
-        <Link to="/tryon/result">falai</Link>
+        <Link to="/home" className="mr-4">Home</Link>
+        <Link to="/tryon" className="mr-4">Try On</Link>
+        <Link to="/saved" className="mr-4">Saved</Link>
+        <Link to="/preferences" className="mr-4">Preferences</Link>
+        <Link to="/tryon/result" className="mr-4">falai</Link>
       </nav>
 
       <Routes>
@@ -42,6 +40,7 @@ function AppRoutes() {
         <Route path="/tryon" element={<TryOn />} />
         <Route path="/tryon/result" element={<TryonResult />} />
         <Route path="/shop" element={<Shop />} />
+        <Route path="/preferences" element={<Preferences />} />
       </Routes>
 
       {!hideNavOn.includes(location.pathname) && <NavBar />}
