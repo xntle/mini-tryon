d1c25243c6959a1689bbd701e39c9f2b589a8464
Akhil Devarasetty <akhild2004@gmail.com>
2025-10-03 09:58:05 -0700

    Refactor routing structure to include Layout component with NavBar and ToastHost

--- fitcheck/src/App.tsx
+++ fitcheck/src/App.tsx
@@ -1,4 +1,4 @@
-import { RouterProvider, createBrowserRouter } from "react-router";
+import { RouterProvider, createBrowserRouter, Outlet } from "react-router";
 import Saved from "./pages/saved";
 import Preferences from "./pages/preferences";
 import NavBar from "./components/Navbar";
@@ -10,47 +10,58 @@ import FullBodyGallery from "./pages/you/page";
 import { ToastHost } from "./lib/toast";
 import FitDetail from "./pages/fit/FitDetail";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <TryOn />,
-  },
-  {
-    path: "/home",
-    element: <Shop />,
-  },
-  {
-    path: "/saved",
-    element: <Saved />,
-  },
-  {
-    path: "/tryon",
-    element: <TryOn />,
-  },
-  {
-    path: "/you",
-    element: <FullBodyGallery />,
-  },
-  {
-    path: "/shop",
-    element: <Shop />,
-  },
-  {
-    path: "/preferences",
-    element: <Preferences />,
-  },
-  {
-    path: "/fit/:id",
-    element: <FitDetail />,
-  },
-]);
-
-export function App() {
+// Layout component that includes NavBar and ToastHost
+function Layout() {
   return (
     <>
-      <RouterProvider router={router} />
+      <Outlet />
       <NavBar />
       <ToastHost />
     </>
   );
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <TryOn />,
+      },
+      {
+        path: "home",
+        element: <Shop />,
+      },
+      {
+        path: "saved",
+        element: <Saved />,
+      },
+      {
+        path: "tryon",
+        element: <TryOn />,
+      },
+      {
+        path: "you",
+        element: <FullBodyGallery />,
+      },
+      {
+        path: "shop",
+        element: <Shop />,
+      },
+      {
+        path: "preferences",
+        element: <Preferences />,
+      },
+      {
+        path: "fit/:id",
+        element: <FitDetail />,
+      },
+    ],
+  },
+]);
+
+export function App() {
+  return <RouterProvider router={router} />;
 }
